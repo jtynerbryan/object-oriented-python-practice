d4878d6fe18da02da1b8ed2ff62176782e05b1bb
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-02-25 19:19:24 -0500

    creating custom built-ins from mutable and immutable datatypes

---
+++ custom_builtin.py
@@ -0,0 +1,27 @@
+# make a version of str that's always reversed
+# remember str's are immutable
+# when creating custom built-ins from an immutable datatype, use new() instead of __inti__()
+# __new__ is a class method, not an instance method and does not take self as an argument
+class ReversedStr(str):
+    def __new__(*args, **kwargs):
+        self = str.__new__(*args, **kwargs)
+        self = self[::-1]
+        return self
+
+import copy
+
+class FilledList(list):
+    def __init__(self, count, value,  *args, **kwargs):
+        super().__init__()
+
+        for _ in range(count):
+            self.append(copy.copy(value))
+
+# __get_attribute__ lets you use . to look up key/values in dicts as you would with a js object
+class JSObject(dict):
+    def __getattribute__(self, item):
+        try:
+            return self[item]
+        except KeyError:
+            # this is calling dict's __get_attribute__() method
+            return super().__getattribute__(item)
