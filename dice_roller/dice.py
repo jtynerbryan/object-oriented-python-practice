989c4570fccdd0e96e419ea854ba0262e150955f
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-02-26 15:23:18 -0500

    added a bunch of magic methods

--- dice_roller/dice.py
+++ dice_roller/dice.py
@@ -10,6 +10,34 @@ class Die:
             raise TypeError("Value must be whole number greater than 0")
         self.value = value or random.randint(1, sides)
 
+    def __int__(self):
+        return self.value
+
+    def __eq__(self, other):
+        return int(self) == other
+
+    def __ne__(self, other):
+        return not int(self) == other
+
+    def __gt__(self, other):
+        return int(self) > other
+
+    def __lt__(self, other):
+        return int(self) < other
+
+    def __ge__(self, other):
+        return int(self) > other or int(self) == other
+
+    def __le__(self, other):
+        return int(self) < other or int(self) == other
+
+    def __add__(self, other):
+        return int(self) + other
+
+    def __radd__(self, other):
+        return int(self) + other        
+
+
 class D6(Die):
     def __init__(self, value=0):
         super().__init__(sides=6, value=value)
